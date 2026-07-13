02a7d5f6dd70c14b03f04bd0ccaa66eb3a828a73
manhdat0110sn <113925174+manhdat0110sn@users.noreply.github.com>
2026-07-13 22:48:46 +0700

    update

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppConfigModule } from './config/app-config.module';
 import { FaceAnalysisModule } from './routes/face-analysis.module';
 import { HealthController } from './routes/health.controller';
 import { ProductsModule } from './routes/products.module';
+import { AuthModule } from './routes/auth.module';
 
 @Module({
-  imports: [AppConfigModule, ProductsModule, FaceAnalysisModule],
+  imports: [AppConfigModule, ProductsModule, FaceAnalysisModule, AuthModule],
   controllers: [HealthController],
 })
 export class AppModule {}
